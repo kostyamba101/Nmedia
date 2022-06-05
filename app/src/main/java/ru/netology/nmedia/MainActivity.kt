d4064173957892ad8fd7ce9892c2a43d46a76988
kostyamba101 <kostyamba101@gmail.com>
2022-06-05 23:29:43 +0400

    04_events

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,56 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            likes = 0,
+            shared = 0
+        )
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            countLikes.text = checkСount(post.likes)
+            countShare.text = checkСount(post.shared)
+
+            like.setOnClickListener {
+                if (post.likedByMe) post.likes-- else post.likes++
+                post.likedByMe = !post.likedByMe
+                countLikes.text = checkСount(post.likes)
+                like.setImageResource(if (post.likedByMe) R.drawable.ic_baseline_favorite_red_24dp else R.drawable.ic_baseline_favorite_24dp)
+            }
+            share.setOnClickListener {
+                post.shared++
+                countShare.text = checkСount(post.shared)
+            }
+        }
+    }
+}
+
+fun checkСount(number: Long): String {
+    val decimalFormat = DecimalFormat("#.#")
+    decimalFormat.roundingMode = RoundingMode.DOWN
+    return when (number) {
+        in 0..999 -> "$number"
+        in 1000..9_999 -> "${decimalFormat.format(number.toFloat() / 1_000)}K"
+        in 10000..999_999 -> "${number / 1_000}K"
+        in 1000000..9_999_999 -> "${decimalFormat.format(number.toFloat() / 1_000_000)}M"
+        else -> "${number / 1_000_000}M"
     }
 }
